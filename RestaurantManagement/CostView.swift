200cf5a1f426348507407cf777c66c6fdddd4822
amrit-banga <115514828+amrit-banga@users.noreply.github.com>
2024-06-25 14:21:15 -0700

    Add files via upload
    added addbusinessview, updated homepageview, updated navigation links

---
+++ RestaurantManagement/CostView.swift
@@ -0,0 +1,22 @@
+//
+//  CostView.swift
+//  RestaurantManagement
+//
+//  Created by Amrit Banga on 6/10/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct CostView: View {
+    var body: some View {
+        Text("Cost Page")
+            .font(.largeTitle)
+            .fontWeight(.bold)
+            .padding()
+    }
+}
+
+#Preview {
+    CostView()
+}
